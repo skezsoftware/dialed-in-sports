3704e312b239247739e6ad34243102354353a6a0
Daniel Vasquez <dwvasquez97@gmail.com>
2024-12-04 17:41:02 -0800

    adjsuted css files and dropdown menu

--- dis-frontend/src/components/header/Header.jsx
+++ dis-frontend/src/components/header/Header.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './header.css';
 import logo from '../../assets/dialed-logo.png';
 
 const Header = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleNavClick = () => {
+    setExpanded(false);  // Close the menu when a link is clicked
+  };
+
   return (
-    <Navbar collapseOnSelect expand="lg" className="navbar-custom" fixed="top">
+    <Navbar 
+      collapseOnSelect 
+      expand="lg" 
+      className="navbar-custom" 
+      fixed="top"
+      expanded={expanded}
+      onToggle={(expanded) => setExpanded(expanded)}
+    >
       <Container>
-        <Navbar.Brand as={Link} to="/">
+        <Navbar.Brand as={Link} to="/" onClick={handleNavClick}>
           <img
             src={logo}
             height="150"
@@ -19,9 +32,9 @@ const Header = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/" className="nav-link">Home</Nav.Link>
-            <Nav.Link as={Link} to="/about" className="nav-link">About</Nav.Link>
-            <Nav.Link as={Link} to="/contact" className="nav-link">Contact</Nav.Link>
+            <Nav.Link as={Link} to="/" onClick={handleNavClick}>Home</Nav.Link>
+            <Nav.Link as={Link} to="/about" onClick={handleNavClick}>About</Nav.Link>
+            <Nav.Link as={Link} to="/contact" onClick={handleNavClick}>Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
